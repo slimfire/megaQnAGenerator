771c2ed396c0b616b6b93052374956727c0c0551
Yohannes <personalprojectyohannes@gmail.com>
2016-01-20 08:15:35 -0600

    [FIX] format file name

--- generator.js
+++ generator.js
@@ -4,20 +4,20 @@ var util = require('./util.js');
 module.exports = {
 	generate : function(path){
 		util.format(path, function(set){
-			fs.writeFile(path + '_questions', set.questions.join(','), 'utf8', function(err){
+			fs.writeFile(path.substr(0, path.length-4) + '_questions.odt', set.questions.join(','), 'utf8', function(err){
 				if(err){
 					console.log("error : ", err);
 				}
 				else{
-					console.log('questions generated !');
+					console.log('Questions generated !');
 				}
 			});
-			fs.writeFile(path + '_questions', set.answers.join(','), 'utf8', function(err){
+			fs.writeFile(path.substr(0, path.length-4) + '_answers.odt', set.answers.join(','), 'utf8', function(err){
 				if(err){
 					console.log("error : ", err);
 				}
 				else{
-					console.log('questions generated !');
+					console.log('Answers generated !');
 				}
 			});
 		});
