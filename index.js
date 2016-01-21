88edb5f9243bc7abe46189cafe395ea4ca15f125
Yohannes <personalprojectyohannes@gmail.com>
2016-01-21 15:13:13 -0600

    [TASK] specify input and output directory

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var generator = require('./generator.js');
+var inputDir = '../Notes/';
+var file = 'ch1.odt';
+var outputDir = '../QnAs/';
 
-generator.generate('ch1.odt')
+generator.generate(file, inputDir, outputDir);
